c5ce181a62d96eb7537f9f3040b2ee23f2efd6a2
unknown <umirzakovasad@gmail.com>
2023-03-22 17:23:01 +0500

    asas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import i18next from 'i18next'
-import { useTranslation } from 'react-i18next'
+// import { useTranslation } from 'react-i18next'
 import Header from './Components/Header/Header'
 import Section from './Components/Section/Section'
 import SectionTwo from './Components/SectionTwo/SectionTwo'
 
 
 export default function App() {
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
 
   // const handleClick = (value) => {
   // i18next.changeLanguage(value)
